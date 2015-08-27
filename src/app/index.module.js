2eaa9064105be15d0bda5961c7578cd50375f447
Paul Julian <bpj101@dameagnesproductions.com>
2015-08-27 02:50:54 -0400

    Initial Commit with Main Page form

---
+++ src/app/index.module.js
@@ -0,0 +1,16 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('ngsocial', [
+      'ngAnimate',
+      'ngCookies',
+      'ngTouch',
+      'ngSanitize',
+      'ngResource',
+      'ui.router',
+      'ui.bootstrap',
+      'ngsocial.facebook'
+    ]);
+
+})();
---
+++ src/app/main/main.controller.js
@@ -0,0 +1,12 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('ngsocial')
+    .controller('MainController', MainController);
+
+  /** @ngInject */
+  function MainController() {
+
+  }
+})();
